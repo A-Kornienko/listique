ede796fce6775f5bc63f87f6820e458ccd46299f
Andrii Korniienko <andrii.korniienko@cudev.org>
2025-04-23 23:28:55 +0300

    test

--- public/index.php
+++ public/index.php
@@ -17,6 +17,4 @@ require __DIR__.'/../vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-dd(111);
-
 $app->handleRequest(Request::capture());
